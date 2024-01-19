625b385b2194e1ea03e657655dd92b8869a00691
joaovitorcamargo <joaocamargo06@gmail.com>
2024-01-18 23:33:26 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from './components/Header/index.vue'
+</script>
+
+<template>
+  <header>
+    <Header />
+  </header>
+  <div>
+    <RouterView/>
+  </div>
+</template>
