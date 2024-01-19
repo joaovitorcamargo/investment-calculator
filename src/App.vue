5beff9d31cdbb40fa969eacea51b234ccfe37596
joaovitorcamargo <joaocamargo06@gmail.com>
2024-01-19 03:37:51 -0300

    Fix: move banner to global component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
 import Header from './components/Header/index.vue'
+import Banner from './components/Banner/index.vue'
 </script>
 
 <template>
   <Header />
-  <main>
+  <Banner/>
+  <main class="investmentData">
     <RouterView/>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.investmentData {
+  padding: 3rem 10rem 3rem 10rem;
+}
+
+@media (max-width: 800px) {
+  .investmentData {
+    padding: 3rem 2rem 3rem 2rem;
+  }
+}
+</style>
---
+++ src/components/Banner/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="banner w-100 flex flex-column gap-3">
+    <h1>Simulador de investimento</h1>
+    <h4>Descubra o quanto você pode economizar.</h4>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$heading-color: #E6E6E6;
+  .banner {
+    padding: 6rem 0 6rem 10rem;
+    background-color: #0B0B0B;
+    h1, h4 {
+      color: $heading-color
+    }
+    h1 {
+      font-size: 3rem;
+      font-weight: bold;
+    }
+    h4 {
+      font-size: 1.35rem;
+      font-weight: lighter;
+    }
+  }
+
+@media (max-width: 800px) {
+  .banner {
+    padding: 6rem 0 6rem 2rem;
+    h1 {
+      font-size: 2rem;
+    }
+    h4 {
+      font-size: 1rem;
+    }
+  }
+}
+</style>
